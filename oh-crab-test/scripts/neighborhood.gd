38066919497ba804c076799bdb33ccb89ae323f0
googie08 <sanojezrel5@gmail.com>
2024-12-28 18:50:03 +0800

    Truck scene uwu

--- oh-crab-test/scripts/neighborhood.gd
+++ oh-crab-test/scripts/neighborhood.gd
@@ -1,24 +1,98 @@
-# neighborhood.gd
 extends Node2D
 
-@onready var dialogue_ui = $Scene2
+@onready var dialogue_ui = $Dialogue
 @onready var interaction_area = $InteractionArea
+@onready var player = $Player
+@onready var truck = $Truck
+
+var player_can_move = true
+var dialogue_active = false
+var has_triggered = false
+var is_truck_leaving = false
+var dialogue_step = 0
 
 func _ready():
-	# Explicitly hide dialogue at start
+	print("DEBUG: _ready called")
 	if dialogue_ui:
 		dialogue_ui.hide_dialogue()
-		
-	# Wait one frame before setting up interactions to ensure everything is initialized
+	
 	await get_tree().create_timer(0.1).timeout
 	
-	# Set up the interaction callbacks
 	if interaction_area:
-		interaction_area.on_interaction = func(): _on_interaction()
-		interaction_area.on_exit = func(): _on_exit()
+		interaction_area.on_interaction = func(): on_interaction()
 
-func _on_interaction():
-	dialogue_ui.trigger_dialogue("Your dialogue text here!")
+func _physics_process(delta):
+	if is_truck_leaving and truck:
+		print("DEBUG: Moving truck")
+		truck.position.x += 100 * delta
+		print("DEBUG: New truck position: ", truck.position)
 
-func _on_exit():
+func _input(event):
+	# Block jump input while dialogue is active OR we're on the last step
+	if dialogue_active or dialogue_step == 5:
+		if event.is_action("jump"):
+			get_viewport().set_input_as_handled()
+			return
+		
+		if event.is_action_pressed("ui_accept"):
+			print("DEBUG: Dialog advance triggered. Current step: ", dialogue_step)
+			end_dialogue()
+			# Prevent the event from propagating after handling dialogue
+			get_viewport().set_input_as_handled()
+
+func on_interaction():
+	if not has_triggered:
+		show_next_dialogue()
+		player_can_move = false
+		dialogue_active = true
+		has_triggered = true
+		
+		if player and player.has_method("set_can_move"):
+			player.set_can_move(false)
+
+func show_next_dialogue():
+	print("DEBUG: Starting dialogue step: ", dialogue_step)
+	match dialogue_step:
+		0:
+			dialogue_ui.trigger_dialogue("(There's the truck!!)")
+		1:
+			dialogue_ui.trigger_dialogue("Hey!! I still have some trash here!")
+		2:
+			print("DEBUG: Reached final dialogue step")
+			start_truck_movement()
+		3: 
+			dialogue_ui.trigger_dialogue("NOOOOO! Mom is gonna kill me T-T")
+		4:
+			dialogue_ui.trigger_dialogue("(I should just go back home)")
+		5:
+			return
+	
+	dialogue_step += 1
+	print("DEBUG: Ending dialogue step: ", dialogue_step)
+
+func start_truck_movement():
+	print("DEBUG: start_truck_movement called")
+	print("DEBUG: Before setting is_truck_leaving: ", is_truck_leaving)
+	is_truck_leaving = true
+	print("DEBUG: After setting is_truck_leaving: ", is_truck_leaving)
+	if truck:
+		print("DEBUG: Truck found at position: ", truck.position)
+	else:
+		print("DEBUG: Truck node is null!")
+
+func end_dialogue():
+	print("DEBUG: end_dialogue called with step: ", dialogue_step)
 	dialogue_ui.hide_dialogue()
+	
+	if dialogue_step < 5:
+		print("DEBUG: Moving to next dialogue")
+		show_next_dialogue()
+	else:
+		start_truck_movement()
+		# Only now do we fully enable player movement and disable dialogue control
+		print("DEBUG: Dialogue complete, enabling player movement")
+		player_can_move = true
+		dialogue_active = false
+		await get_tree().create_timer(0.1).timeout  # Small delay before enabling movement
+		if player and player.has_method("set_can_move"):
+			player.set_can_move(true)
--- oh-crab-test/scripts/player.gd
+++ oh-crab-test/scripts/player.gd
@@ -1,45 +1,54 @@
 #player.gd
-
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+
 const SPEED = 60.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var can_move = true  # Add this variable to control movement
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Only process movement inputs if can_move is true
+	if can_move:
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Update animation based on movement and jump state
-	if not is_on_floor():
-		if direction > 0:
-			animated_sprite.play("jump_right")
+		# Update animation based on movement and jump state
+		if not is_on_floor():
+			if direction > 0:
+				animated_sprite.play("jump_right")
+			elif direction < 0:
+				animated_sprite.play("jump_left")
+			else:
+				animated_sprite.play("idle")
+		elif direction > 0:
+			animated_sprite.play("walk_right")
 		elif direction < 0:
-			animated_sprite.play("jump_left")
+			animated_sprite.play("walk_left")
 		else:
 			animated_sprite.play("idle")
-	elif direction > 0:
-		animated_sprite.play("walk_right")
-	elif direction < 0:
-		animated_sprite.play("walk_left")
 	else:
+		# When movement is disabled, stop horizontal movement
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Still play idle animation when stopped
 		animated_sprite.play("idle")
-
+	
+	# Always apply movement (for gravity and stopping)
 	move_and_slide()
 
 func _on_coin_body_entered(_body: Node2D) -> void:
@@ -47,3 +56,7 @@ func _on_coin_body_entered(_body: Node2D) -> void:
 
 func _ready():
 	add_to_group("player")
+
+# Add this function to control movement from outside
+func set_can_move(value: bool):
+	can_move = value
