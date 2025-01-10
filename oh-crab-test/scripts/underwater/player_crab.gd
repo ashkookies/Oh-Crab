ff3c861dd063e7205877cb57c0be6c0131b10bb4
rawr <66loneliness99@gmail.com>
2025-01-11 03:49:37 +0800

    latest underwater

--- oh-crab-test/scripts/underwater/player_crab.gd
+++ oh-crab-test/scripts/underwater/player_crab.gd
@@ -1,38 +1,44 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var dialogue_node = $"../Dialogue"
+
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-
-func player():
-	pass
+var is_movement_locked: bool = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if is_movement_locked:
+		velocity.x = 0
+		animated_sprite.play("idle")
+		move_and_slide()
+		return
+
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if direction != 0:
 		if direction > 0:
 			animated_sprite.play("walking_right")
 		else:
 			animated_sprite.play("walking_left")
 	else:
-		animated_sprite.play("idle")
 		velocity.x = 0
+		animated_sprite.play("idle")
 	
-				
 	move_and_slide()
+
+func lock_movement():
+	is_movement_locked = true
+
+func unlock_movement():
+	is_movement_locked = false
